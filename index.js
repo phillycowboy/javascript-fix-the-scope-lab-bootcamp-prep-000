4da15a94b705c15c3ef9f17b7f06967c2717504c
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-03 18:36:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
   var animal = 'dog';
-  return (animal);
+  console.log(animal);
 }
 
 function add2(n) {
